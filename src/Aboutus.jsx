f1a4dd1218207479a6caf610dd85cecf8cfe767b
SanjotKaur9102 <sanjotkaur9102@gmail.com>
2024-02-23 19:44:39 +0530

    function

---
+++ src/Aboutus.jsx
@@ -0,0 +1,10 @@
+//make component type rfc...automatically aa jaata hai
+import React from 'react'
+
+export default function Aboutus() {
+  return ( 
+
+    
+    <div>welcome to mern stack</div>
+  )
+}
